60d4a5ef3b1f222e7407260bff550fe7211508dd
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2019-11-25 22:37:13 +0000

    console.assert implemented

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🛑🛑🛑Assersion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✅✅✅Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual(1, 2);
+
+//🛑🛑🛑 Assertion Failed: "Lighthouse Labs" !== "Bootcamp"
+//✅✅✅ Assertion Passed: 1 === 1
