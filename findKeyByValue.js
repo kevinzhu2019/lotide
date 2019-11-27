a44518d854f3b2be1859375f61806f7ceccbcb0d
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2019-11-27 22:06:23 +0000

    Adding findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assersion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(transferInObj, name) {
+  // console.log(transferInObj);
+  for (const i in transferInObj) {
+    // console.log(transferInObj[i]);
+    if (transferInObj[i] === name) {
+      return i
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// findKeyByValue(bestTVShowsByGenre, "The Wire");
