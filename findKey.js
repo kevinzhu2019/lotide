e1a55b62baa04e14176a97798b621dfd39ad177f
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2019-11-28 22:35:15 +0000

    correct the problem of not using callback in findKey.js

--- findKey.js
+++ findKey.js
@@ -8,17 +8,25 @@ const assertEqual = function(actual, expected) {
 
 const findKey = function(takeinObj, callback) {
   for (const item in takeinObj) {
-    if (takeinObj[`${item}`]["stars"] === 2) {
-      return `${item}`;
+    if (callback(takeinObj[`${item}`])) {
+      return item;
     }
   }
 };
 
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma"); // => "noma"
+// assertEqual(findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2), "noma"); // => "noma"
+console.log(findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2));
